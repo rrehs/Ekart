83f816b5a6c3ac3c085c8a6faa0ab491ef29eced
rrehs <spinorager338@gmail.com>
2024-10-01 11:35:04 +0800

    removed useless code

--- src/main/java/com/reljicd/ShoppingCartApplication.java
+++ src/main/java/com/reljicd/ShoppingCartApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShoppingCartApplication {
 
     public static void main(String[] args) {
-        System.out.println("code is now running");
+        
         SpringApplication.run(ShoppingCartApplication.class, args);
     }
 }
