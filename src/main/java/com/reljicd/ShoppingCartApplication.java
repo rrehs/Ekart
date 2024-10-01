8a4ab65301ef3b75232ee9a1b7bf31e5b0bf9a29
rrehs <spinorager338@gmail.com>
2024-10-01 11:47:56 +0800

    removed useless comment

--- src/main/java/com/reljicd/ShoppingCartApplication.java
+++ src/main/java/com/reljicd/ShoppingCartApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShoppingCartApplication {
 
     public static void main(String[] args) {
-        //this is a comment
+       
         SpringApplication.run(ShoppingCartApplication.class, args);
     }
 }
