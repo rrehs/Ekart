e7685631b85dfa5fb242b9c1489fd7a78fdc7b90
rrehs <spinorager338@gmail.com>
2024-10-02 11:11:45 +0800

    broken code

--- src/main/java/com/reljicd/ShoppingCartApplication.java
+++ src/main/java/com/reljicd/ShoppingCartApplication.java
@@ -8,7 +8,7 @@ public class ShoppingCartApplication {
 
     public static void main(String[] args) {
        
-       
+       prposefully broken line of code
         SpringApplication.run(ShoppingCartApplication.class, args);
     }
 }
