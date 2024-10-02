8fba946e8d2775d6131ec5510d6e88a310d30f7b
rrehs <spinorager338@gmail.com>
2024-10-02 11:03:52 +0800

    removed the broken code

--- src/main/java/com/reljicd/ShoppingCartApplication.java
+++ src/main/java/com/reljicd/ShoppingCartApplication.java
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShoppingCartApplication {
 
     public static void main(String[] args) {
-       System.out.println(purposefullybrokencode)
-       //broken code to test
+       
+       
         SpringApplication.run(ShoppingCartApplication.class, args);
     }
 }
