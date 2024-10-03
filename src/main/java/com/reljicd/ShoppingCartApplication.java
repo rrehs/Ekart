7f47f3ddf3502b6bb955d3f70e67a4985ab1aa9f
rrehs <spinorager338@gmail.com>
2024-10-03 09:47:59 +0800

    new comment to test

--- src/main/java/com/reljicd/ShoppingCartApplication.java
+++ src/main/java/com/reljicd/ShoppingCartApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShoppingCartApplication {
 
     public static void main(String[] args) {
+        //test again
         SpringApplication.run(ShoppingCartApplication.class, args);
     }
 }
