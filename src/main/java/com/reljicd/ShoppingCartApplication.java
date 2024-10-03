93a13f3f2baeadbf0f3fa80bbe02185d3ae63a63
rrehs <spinorager338@gmail.com>
2024-10-03 12:08:57 +0800

    testing new jenkinsfile

--- src/main/java/com/reljicd/ShoppingCartApplication.java
+++ src/main/java/com/reljicd/ShoppingCartApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShoppingCartApplication {
 
     public static void main(String[] args) {
+        //another test
         SpringApplication.run(ShoppingCartApplication.class, args);
     }
 }
