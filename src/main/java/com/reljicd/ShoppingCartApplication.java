0eb6d495d102995c240145437b589c4ce12f1c38
rrehs <spinorager338@gmail.com>
2024-10-03 09:43:56 +0800

    removed comment

--- src/main/java/com/reljicd/ShoppingCartApplication.java
+++ src/main/java/com/reljicd/ShoppingCartApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShoppingCartApplication {
 
     public static void main(String[] args) {
-       
-       //new comment testing jenkins new version
         SpringApplication.run(ShoppingCartApplication.class, args);
     }
 }
